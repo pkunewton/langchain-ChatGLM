74d68701301d2c5ef44f4aa5d7cd93d78f50266b
liuke <tinypig0320@gmail.com>
2023-07-13 10:18:46 +0800

    换key

--- models/fastchat_openai_llm.py
+++ models/fastchat_openai_llm.py
@@ -116,7 +116,7 @@ class FastChatOpenAILLM(RemoteRpcModel, LLM, ABC):
         try:
             import openai
             # Not support yet
-            openai.api_key = "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"
+            openai.api_key = "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"
             openai.api_base = self.api_base_url
         except ImportError:
             raise ValueError(
